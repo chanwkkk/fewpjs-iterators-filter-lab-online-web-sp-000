6434696c51e7cb3cae393d132c687c47cf8722e7
Yingqi Chen <chanwk18@hotmail.com>
2020-01-02 22:57:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,14 @@
 function findMatching(array,name){
   return array.filter(item=>item.toUpperCase()==name.toUpperCase())
 }
+
+function fuzzyMatch(array,string){
+  return array.filter(item=>{
+  
+    let length =   string.split("").length
+    let testString = []
+    for (let i = 0;i<length-1; i++){
+      testString.push(item[i])
+    }
+  })
+}
