44f90afdd3a5c4879b266ce49d7a5185cf4f5fd6
Yingqi Chen <chanwk18@hotmail.com>
2020-01-02 23:08:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,15 +5,20 @@ function findMatching(array,name){
   return array.filter(item=>item.toUpperCase()==name.toUpperCase())
 }
 
+function matchingTest(item,testWord){
+  let length = testWord.split("").length
+  let testString = []
+  for (let i = 0;i<length-1; i++){
+    testString.push(item[i])
+  }
+  let test = testString.join("")
+  return !!(test===testWord)
+
+}
+
+
 function fuzzyMatch(array,string){
   return array.filter(item=>{
 
-    let length =   string.split("").length
-    let testString = []
-    for (let i = 0;i<length-1; i++){
-      testString.push(item[i])
-    }
-    let test = testString.join("")
-    return test==string
   })
 }
